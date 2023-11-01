a84be9e613ace7276cf3e0c27d2cb475e0933213
DASOM <n980805@gmail.com>
2023-11-01 12:04:15 +0900

    🌱 스프링 핵심 원리 - 기본편

---
+++ Spring Core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) // http 요청 당 하나씩 빈 생성, http 요청이 끝나는 시점에 빈 소멸
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    // 빈이 생성되는 시점에 알 수 없으므로 외부에서 setter로 입력 받음
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
