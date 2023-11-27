d6a5c86415444acfde0ae3f28ebf233155227c08
DASOM <n980805@gmail.com>
2023-11-27 10:58:34 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술

---
+++ Spring MVC/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,44 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    // 싱글톤 생성
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    // 아무나 생성할 수 없도록 private로 막음
+    private MemberRepository() {
+    }
+
+    // 회원 저장
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    // 회원 아이디로 찾기
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    // 회원 목록 조회
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
