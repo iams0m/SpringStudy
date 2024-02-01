cd7d84c8e1ae200b50c44ac261251da7dbb31dfd
DASOM <n980805@gmail.com>
2024-02-01 22:24:27 +0900

    feat: 세션 정보 확인 컨트롤러 추가

---
+++ Spring MVC2/login/src/main/java/hello/login/web/session/SessionInfoController.java
@@ -0,0 +1,37 @@
+package hello.login.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        // 세션이 없는 경우
+        if (session == null) {
+            return "세션이 없습니다.";
+        }
+
+        // 세션이 있는 경우, 세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name)));
+
+        // 세션이 기본적으로 제공하는 데이터
+        log.info(("sessionId={}"), session.getId());
+        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", new Date(session.getCreationTime()));
+        log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
+        log.info("isNew={}", session.isNew());
+
+        return "세션 출력";
+    }
+}
